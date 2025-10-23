81fc36d52c6cf3a78c1abbf5e631b4507e38cfd0
Dorian L. <gyos23@users.noreply.github.com>
2025-10-23 08:01:53 +0100

    Set up React application with main component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ProjectManagementPlatform from './App'; // Note: importing our component
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ProjectManagementPlatform />
+  </React.StrictMode>
+);
